09df2d29aef668a6bc3be82b8ecd72c7a6a576ad
gsalaman <g.salaman@comcast.net>
2019-08-13 09:58:08 -0600

    repeating sample tests

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pi_audio_simple
+
+Looks like pyaudio collides with the matrix code for pulse generation.
+Since we're running a real OS, I think I can have one program (process?) 
+generating the sound data, and another using the matrix to display it!
+
+
--- sample_repeat.py
+++ sample_repeat.py
@@ -2,6 +2,8 @@ import pyaudio
 import struct
 #import numpy as np
 
+import time
+
 CHUNK = 1024 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -11,8 +13,14 @@ p = pyaudio.PyAudio()
 
 stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input_device_index=DEV_INDEX,input=True,output=False,frames_per_buffer=CHUNK)
 
-data = stream.read(CHUNK)
-
-data_int = struct.unpack(str(CHUNK) +'h', data)  #unpack as 16-bit signed int.
-print(data_int)
+try:
+  print("Hit ctl-c to exit")
+  while (True):
+    data = stream.read(CHUNK,exception_on_overflow = False)
+    data_int = struct.unpack(str(CHUNK) +'h', data)  
+    print(data_int)
+    time.sleep(1)
+    
+except KeyboardInterrupt:
+  exit(0)
 
